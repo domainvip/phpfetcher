cb0dfc9ab25e154ca0efcd325a19107d52b4ab9d
Liu Yongjun <ceo@ebuy365.com>
2016-08-12 11:28:58 +0800

    rebeca

--- demo/crawl_baidu_page.php
+++ demo/crawl_baidu_page.php
@@ -2,19 +2,20 @@
 //下面两行使得这个项目被下载下来后本文件能直接运行
 $demo_include_path = dirname(__FILE__) . '/../';
 set_include_path(get_include_path() . PATH_SEPARATOR . $demo_include_path);
-
+$posturl = "http://dev.ebuy365.com/site/add";
+$post_data = array ("url" => "$res[$i]","search_words" => "test");
 require_once('phpfetcher.php');
 class mycrawler extends Phpfetcher_Crawler_Default {
     public function handlePage($page) {
         //打印处当前页面的title
-        $res = $page->sel('//h3/a');
+        $res = $page->sel('//h2/a');
         for ($i = 0; $i < count($res); ++$i) {
-            echo $res[$i]->plaintext;
-            echo "\n";
-            echo $res[$i]->getAttribute('href');
-            echo "\n";
-            echo "\n";
+           // echo $res[$i]->plaintext;
+            //echo "\n";
+              $res[$i]->getAttribute('href');
+
         }
+        echo $res[0]->getAttribute('href');
     }
 }
 
@@ -23,7 +24,7 @@ $arrJobs = array(
     //任务的名字随便起，这里把名字叫qqnews
     //the key is the name of a job, here names it qqnews
     'qqnews' => array( 
-        'start_page' => 'http://www.baidu.com/s?wd=facebook', //起始网页
+        'start_page' => 'http://www.rebeccaminkoff.com/shoes/new-arrivals/', //起始网页
         'link_rules' => array(
             /*
              * 所有在这里列出的正则规则，只要能匹配到超链接，那么那条爬虫就会爬到那条超链接
